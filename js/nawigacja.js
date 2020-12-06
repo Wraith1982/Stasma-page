7be9546cc81d8286cb5fd603561f54699d357f35
Wraith1982 <rattlehead20@poczta.onet.pl>
2020-12-06 17:52:31 +0100

    Add files via upload

---
+++ js/nawigacja.js
@@ -0,0 +1,20 @@
+// Silesian V
+
+function openSilesianV() {
+    document.getElementById("SilesianV").style.height = "100%";
+}
+
+function closeSilesianV() {
+    document.getElementById("SilesianV").style.height = "0%";
+}
+
+
+// Silesian IV
+
+function openSilesianIV() {
+    document.getElementById("SilesianIV").style.width = "100%";
+}
+
+function closeSilesianIV() {
+    document.getElementById("SilesianIV").style.width = "0%";
+}
